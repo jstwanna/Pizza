e41f00b0e893cd93f0f6d2fd91d9e735d688cf69
Антон Антон <justwant6@mail.ru>
2024-07-12 11:38:23 +0300

    feat(api): добавит апи проект

--- Pizza.AppHost/Program.cs
+++ Pizza.AppHost/Program.cs
@@ -1,3 +1,14 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var api = builder
+    .AddProject<Projects.Pizza_Api>("pizza-api")
+    .WithExternalHttpEndpoints();
+
+var frontend = builder
+    .AddNpmApp("pizza-frontend", "../Pizza.Frontend", "dev")
+    .WithReference(api)
+    .WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
+
 builder.Build().Run();
