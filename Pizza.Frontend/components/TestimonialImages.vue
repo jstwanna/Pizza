a5d2c606b8fd5d8a27a6c3df2c1476498b5f7f59
Kararakt <xom-vlaskin0@outlook.com>
2024-07-19 19:07:13 +0300

    feat: добавит анимацию появления фотографий на странице О нас

--- Pizza.Frontend/components/TestimonialImages.vue
+++ Pizza.Frontend/components/TestimonialImages.vue
@@ -9,18 +9,56 @@ interface Props {
 withDefaults(defineProps<Props>(), {
   isMan: false,
 });
+
+const imagesContainerRef = ref<HTMLElement | null>(null);
+const imageRefs = ref<HTMLImageElement[]>([]);
+
+const setImageRef = (el: HTMLImageElement) => {
+  if (el) {
+    imageRefs.value.push(el);
+  }
+};
+
+const handleScroll = () => {
+  if (!imagesContainerRef.value) return;
+
+  const halfScreenHeight = window.innerHeight / 1.25;
+  const containerTop = imagesContainerRef.value.getBoundingClientRect().top;
+
+  imageRefs.value.forEach((img) => {
+    if (img && containerTop < halfScreenHeight) {
+      img.classList.add('testimonial-images__image_type_animate');
+    }
+  });
+};
+
+onMounted(() => {
+  nextTick(() => {
+    window.addEventListener('scroll', handleScroll);
+    handleScroll();
+  });
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener('scroll', handleScroll);
+});
 </script>
 
 <template>
   <div
     :class="['testimonial-images', { 'testimonial-images_type_man': isMan }]"
+    ref="imagesContainerRef"
   >
     <img
       v-for="image in images.images"
       :src="image.src"
       :key="image.id"
       :alt="image.title"
-      class="testimonial-images__image"
+      :ref="(el) => setImageRef(el as HTMLImageElement)"
+      :class="[
+        'testimonial-images__image',
+        { 'testimonial-images__image_type_man': image.id === 1 },
+      ]"
     />
   </div>
 </template>
@@ -41,7 +79,22 @@ withDefaults(defineProps<Props>(), {
     width: 30%;
     position: absolute;
     right: 0;
+    opacity: 0;
     bottom: 0.9375rem;
+    transform: translate(0, -15%);
+    transition: transform 0.45s cubic-bezier(0.6, 0.2, 0.1, 1) 0s,
+      opacity 0.5s cubic-bezier(0.6, 0.2, 0.1, 1) 0s;
+
+    &_type {
+      &_man {
+        transform: translate(-15%, 0);
+      }
+
+      &_animate {
+        transform: translate(0, 0);
+        opacity: 1;
+      }
+    }
   }
 }
 </style>
