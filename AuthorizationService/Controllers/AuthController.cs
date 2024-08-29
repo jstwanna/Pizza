dd6a99f01298b824e29effd8c08e8e7f4c8f2122
Антон Антон <justwant6@mail.ru>
2024-08-29 21:31:48 +0300

    feat(identity): добавит удалит проект авторизации

---
+++ AuthorizationService/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthorizationService.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public AuthController (SignInManager<IdentityUser> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+
+        [HttpPost]
+        public void auth ()
+        {
+            signInManager.UserManager.CreateAsync(new IdentityUser
+            {
+                UserName = "213"
+            });
+        }
+    }
+}
---
+++ AuthorizationService/Data/AuthDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthorizationService.Data
+{
+    public class AuthDbContext : IdentityDbContext<IdentityUser>
+    {
+        AuthDbContext(DbContextOptions<AuthDbContext> options) 
+            : base(options) { }
+    }
+}
